4cc69fc3fbc6ba5999a4571ad4953ed70691b66f
Apoteotico <eitel.trabajo@gmail.com>
2023-11-20 04:44:50 -0300

    creación del token listo- proximo creacion de login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,32 @@
 import User from '../models/usesr.model.js'
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from '../libs/jwt.js';
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body
 
   try {
+    const passwordHash = await bcrypt.hash(password, 10) // hash
+
     const newUser = new User({
       username,
       email,
-      password
-    })
-    const saveUser = await newUser.save();
-    res.json(saveUser);
+      password: passwordHash,
+    });
+
+    const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie('token', token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updateAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error)
+    res.status(500).json({ message: error.message });
   }
 
 };
